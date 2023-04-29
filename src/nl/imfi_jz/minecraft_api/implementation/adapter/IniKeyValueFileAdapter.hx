013360f4d185ed8ed5813aa20a22bc3dcbcce319
Imperfiction <10811551+imfi-jz@users.noreply.github.com>
2023-04-29 10:07:21 +0200

    Refactor to comply with Haxe update (4.2 to 4.3)

--- src/nl/imfi_jz/minecraft_api/implementation/adapter/IniKeyValueFileAdapter.hx
+++ src/nl/imfi_jz/minecraft_api/implementation/adapter/IniKeyValueFileAdapter.hx
@@ -58,7 +58,7 @@ class IniKeyValueFileAdapter extends IniFile implements KeyValueFile<String> {
     public function setValue(key:String, value:String):Bool {
         var set = false;
         // Look for root keys to replace value
-        for(aKey in super.keys){
+        for(aKey in this.keys){
             if(aKey.key == key){
                 aKey.value = Std.string(value);
                 set = true;
@@ -68,7 +68,7 @@ class IniKeyValueFileAdapter extends IniFile implements KeyValueFile<String> {
 
         if(!set){
             // Look for section keys to replace value
-            for(section in super.sections){
+            for(section in this.sections){
                 for(aKey in section.keys){
                     if(aKey.key == key){
                         aKey.value = Std.string(value);
@@ -84,7 +84,7 @@ class IniKeyValueFileAdapter extends IniFile implements KeyValueFile<String> {
         
         if(!set) {
             // Add key/value to root
-            super.keys.add(new Key(key, Std.string(value)));
+            this.keys.add(new Key(key, Std.string(value)));
             set = true;
         }
         
@@ -100,7 +100,7 @@ class IniKeyValueFileAdapter extends IniFile implements KeyValueFile<String> {
     /** Gets all available keys in this ini file. Looks both in root and sections. **/
 	public function getKeys():StandardCollection<String> {
         final keyStrings = getRootKeys();
-        for(section in super.sections){
+        for(section in this.sections){
             for(aKey in section.keys){
                 keyStrings.push(aKey.key);
             }
@@ -110,14 +110,14 @@ class IniKeyValueFileAdapter extends IniFile implements KeyValueFile<String> {
     /** Gets the available keys in the root (first) section of this ini file. **/
     public function getRootKeys():StandardCollection<String> {
         final keyStrings = new StandardCollection<String>();
-        for(key in super.keys){
+        for(key in this.keys){
             keyStrings.push(key.key);
         }
         return keyStrings;
     }
     /** Gets the available keys under the section name `sectionName`. **/
     public function getSectionKeys(sectionName:String):StandardCollection<String> {
-        final section = super.sections.filter(s -> s.name == sectionName).first();
+        final section = this.sections.filter(s -> s.name == sectionName).first();
         final keyStrings = new StandardCollection<String>();
         for(key in section.keys){
             keyStrings.push(key.key);
@@ -135,11 +135,11 @@ class IniKeyValueFileAdapter extends IniFile implements KeyValueFile<String> {
     }
     /** Adds a section to this ini file with name `sectionName` and key/value pairs `content`. **/
     public function addSection(sectionName:String, content:Map<String, String>):Bool {
-        final section = new Section(sectionName, super.lineSeparator);
+        final section = new Section(sectionName, this.lineSeparator);
         for(entry in content.keyValueIterator()){
             section.keys.add(new Key(entry.key, entry.value));
         }
-        super.sections.add(section);
+        this.sections.add(section);
 
         if(isAutosaving()){
             return save();
@@ -148,8 +148,8 @@ class IniKeyValueFileAdapter extends IniFile implements KeyValueFile<String> {
     }
     /** Removes the first section found with name `sectionName`. This also removes all underlying keys. **/
     public function removeSection(sectionName:String):Bool {
-        final section = super.sections.filter(s -> s.name == sectionName).first();
-        final success = super.sections.remove(section);
+        final section = this.sections.filter(s -> s.name == sectionName).first();
+        final success = this.sections.remove(section);
 
         if(success && isAutosaving()){
             return save();
@@ -159,7 +159,7 @@ class IniKeyValueFileAdapter extends IniFile implements KeyValueFile<String> {
     /** Returns the content of section `sectionName` as a key/value map. **/
     public function getSectionContent(sectionName:String):Map<String, String> {
         final content = new Map<String, String>();
-        for(key in super.sections.filter(s -> s.name == sectionName).first().keys){
+        for(key in this.sections.filter(s -> s.name == sectionName).first().keys){
             content.set(key.key, key.value);
         }
         return content;
@@ -167,7 +167,7 @@ class IniKeyValueFileAdapter extends IniFile implements KeyValueFile<String> {
 
     /** Returns the name of the ini file. **/
     public function getName():String {
-        return super.name;
+        return this.name;
     }
 }
 #end
